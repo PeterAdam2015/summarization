b4271acb70c5cd83903cd2cd3e345100b985cf1a
PeterAdam2015 <chx245296678@gmail.com>
2018-09-13 23:58:31 +0800

    some little changes

--- utils/create_datasets.py
+++ utils/create_datasets.py
@@ -7,3 +7,26 @@ import torch
 from torch.utils.data import Dataset
 from utils.create_vocab import preprocess
 
+def create_datesets():
+    """
+    Args:
+        the high level function to create a h5py file,
+        mode can be either train, val or test, for the 
+        train mode, one can set the datasets to be a content
+        and each word will be mapping to a index, if the word
+        not in the vocabulary, then set skip this or set to
+        Unkow regard to the model's setting. 
+    """
+    pass
+
+def word2id(vocab, sentence, skip=True):
+    ids=[]
+    for word in sentence:
+        try:
+            ids.append(vocab[word])
+        except KeyError:
+            if skip:
+                pass
+
+
+            
