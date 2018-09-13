44870db2a67ecb4e45637862600e4f601ecab97b
PeterAdam2015 <chx245296678@gmail.com>
2018-09-13 21:46:27 +0800

    add tokenizer tools

--- utils/create_vocab.py
+++ utils/create_vocab.py
@@ -2,15 +2,37 @@ import pandas as pd
 import os
 import h5py
 import re
+from nltk.tokenize import RegexpTokenizer
 """
 create a simple vocab files, it will be stored in the ../data folder with a
 json file. This script will be the baseline file to create the Voacb. 
 """
 
-def create_vocab(datapath = None):
+def find_all_vocabs(file_path):
     """
     Args:
-        datapath, will be the processed data, like we have use the re 
-        module to sub some unwanted punctuius
+        file_path: a csv file in  the ../data folder
     """
+    content_words=[]
+    title_words=[]
+    data = pd.read_csv(file_path, encoding='utf-8')
+    for item in list(data.content):
+        tokens=list(preprocess(item))
+        content_words.extend(tokens)
+
+    for item in list(data.title):
+        tokens=list(preprocess(item))
+        title_words.extend(tokens)
+    all_words = content_words+title_words
+    del content_words, title_words
+    return all_words
+
+
+
+def preprocess(sentences):
+    sentences = sentences.lower()
+    tokenizer = RegexpTokenizer(r'w\+')
+    tokens = tokenizer.tokenize(sentences)
+    filtered_words = list(tokens)
+    return filtered_words
 
